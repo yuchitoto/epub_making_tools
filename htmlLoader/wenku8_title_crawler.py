5f13d8621b11e741cfe9cef6578b05c9b2013304
Yu Chi To <yuchitoto@gmail.com>
2020-09-28 19:27:34 +0800

    update crawler for multithread settings

--- htmlLoader/wenku8_title_crawler.py
+++ htmlLoader/wenku8_title_crawler.py
@@ -2,10 +2,11 @@ import requests as req
 from bs4 import BeautifulSoup
 import re
 import csv
-import sys
+import sys, getopt
 from wenku8_classes import wenku8
 from bookDB import db
 from multiprocessing import Pool
+import multiprocessing
 
 tag_finde = re.compile("<[^>]*>")
 date_finde = re.compile("：".encode('gbk').decode('latin-1'))
@@ -45,6 +46,19 @@ def crawl(obj):
 
 
 if __name__ == "__main__":
+    thread = multiprocessing.cpu_count()
+
+    try:
+        arg, val = getopt.getopt(sys.argv[1:], "t:", ["threads"])
+    except getopt.error as err:
+        print(str(err))
+        sys.exit(-1)
+
+    for ca, cv in arg:
+        if ca in ("-t", "--threads"):
+            thread = int(cv)
+            print("Using {} threads".format(cv))
+
     usedID = wenku8.containedID()
     flag = True
     cur = 1
@@ -64,47 +78,6 @@ if __name__ == "__main__":
     new_entries = list()
     update_entries = list()
     failed = 0
-
-    thread = 12
-
-    """
-    while failed < fail_lim:
-        if mode == 'find new' and cur in usedID:
-            cur += 1
-            failed = 0
-            continue
-
-        print("load wenku8 book id={}".format(cur))
-        res = req.get("https://www.wenku8.net/book/{}.htm".format(cur))
-        soup = BeautifulSoup(res.text, 'html.parser', multi_valued_attributes=None)
-        title = soup.title.encode('latin-1')
-        if "出现错误".encode('gbk') in title:
-            #print("https://www.wenku8.net/book/{}.htm".format(cur))
-            failed += 1
-            print('failed: {}'.format(cur))
-        else:
-            #print(title.decode('gbk'))
-            tmp = list(map(lambda x: x.strip(" "), tag_finde.sub("", title.decode('gbk')).split("-")))
-            flag = 'true'
-            for i in soup.findAll('b'):
-                cmper = str(i).encode('latin-1')
-                if finder in cmper:
-                    flag = 'false'
-
-            last_update = None
-            if flag == 'true':
-                last_update = date_finde.split(str(soup.findAll('td', {'width':'20%'})[3]))[1]
-            entry = {'id':cur, 'book_name':tmp[0], 'author':tmp[1], 'publisher':tmp[2], 'available':flag, 'last_update':last_update}
-
-            if mode == 'update' and cur in usedID:
-                update_entries.append(entry)
-            else:
-                new_entries.append(entry)
-
-            failed = 0
-
-        cur+=1
-    """
 
     with Pool(processes=thread) as pool:
         if cur <= usedID[-1]:
